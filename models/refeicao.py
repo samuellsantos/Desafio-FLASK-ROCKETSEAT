5c52eefd04743c7036e5213d06a3014b9e201f20
Samuel Santos <samuka0102128@gmail.com>
2025-03-03 20:52:35 -0300

    setting up the project

---
+++ models/refeicao.py
@@ -0,0 +1,9 @@
+from database import db
+from flask_login import UserMixin
+
+class Refeicao(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80), nullable=False)
+    descricao = db.Column(db.String(80), nullable="False")
+    data_hora = db.Column(db.DateTime, nullable="False")
+    dieta = db.Column(db.Boolean, nullable="False") 
