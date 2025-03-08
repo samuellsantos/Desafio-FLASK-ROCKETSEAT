9ff80ab32a70a6389209e31cccc06d34e60e5fd5
Samuel Santos <samuka0102128@gmail.com>
2025-03-08 15:51:39 -0300

    Add refeicao

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from models.refeicao import Refeicao
 from models.User import User
 from database import db
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -29,7 +30,7 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
             login_user(user)
-            print(current_user.is_authenticated)
+            print(current_user)
             return jsonify({"message": "Credenciais enviadas com sucesso!"})
     return jsonify({"message": "Credênciais inválidas."}), 400
 
@@ -39,6 +40,27 @@ def login():
 def logout():
     logout_user()
     return jsonify({"message": "Logout realizado com sucesso."})
+
+@app.route('/cadastrar', methods=["POST"])
+@login_required
+def add_refeicao():
+    data = request.json
+    descricao = data.get("descricao")
+    data_hora_str = data.get("data_hora")
+    data_hora = datetime.strptime(data_hora_str, '%d-%m-%y %H:%M')
+    dieta = data.get("dieta")
+    
+    if descricao and data_hora and dieta is not None:
+        refeicao = Refeicao(nome=current_user,
+                            descricao=descricao, 
+                            data_hora=data_hora,
+                             dieta=dieta)
+        refeicao = Refeicao(nome=current_user.username, descricao=descricao, data_hora=data_hora, dieta=dieta)
+        db.session.add(refeicao)
+        db.session.commit()
+        return jsonify({"message": "Refeição adicionada com sucesso!"})
+    return jsonify({"message": "Dados inválidos"}), 401
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/refeicao.py
+++ models/refeicao.py
@@ -4,6 +4,6 @@ from flask_login import UserMixin
 class Refeicao(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(80), nullable=False)
-    descricao = db.Column(db.String(80), nullable="False")
-    data_hora = db.Column(db.DateTime, nullable="False")
-    dieta = db.Column(db.Boolean, nullable="False") 
+    descricao = db.Column(db.String(80), nullable=False)
+    data_hora = db.Column(db.DateTime, nullable=False)
+    dieta = db.Column(db.Boolean, nullable=False) 
